532176116f0dc1dfd5445b6e17cd96fc7e0c9d91
Jay Kumar <jaykch@outlook.com>
2016-11-18 18:12:55 +0000

    Finished final version previous version wasn't final as I forgot to have an input state for the text

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -6,7 +6,20 @@ import RawMarkdown from "./markdown/RawMarkdown"
 
 export default class Layout extends React.Component {
 
-    text = 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.*';
+    constructor() {
+        super();
+
+        this.state = {
+            text: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.*'
+        }
+    }
+
+    changeHandler(val){
+        this.setState({
+            text: val
+        });
+    }
+
 
     render() {
         return (
@@ -14,10 +27,10 @@ export default class Layout extends React.Component {
                 <h1 class="text-center header">Markdown Previewer</h1>
                 <div class="row">
                     <div class="col-md-6 raw-text">
-                        <RawMarkdown text={this.text}/>
+                        <RawMarkdown text={this.state.text} changeText={this.changeHandler.bind(this)}/>
                     </div>
                     <div class="col-md-6 marked-text">
-                        <ConvertedMarkdown textToConvert={this.text}/>
+                        <ConvertedMarkdown textToConvert={this.state.text}/>
                     </div>
                 </div>
                 <h1 class="text-center credits">Built By <a href="http://www.jaykch.com/" target="_blank">Jay Kumar</a></h1>
--- src/js/components/markdown/RawMarkdown.js
+++ src/js/components/markdown/RawMarkdown.js
@@ -1,10 +1,20 @@
 import React from "react"
 
 export default class ConvertedMarkdown extends React.Component {
+//dangerouslySetInnerHTML={{__html: this.props.text.split("\n").join("<br>")}}
+
+    updateText(e){
+        const text = e.target.value;
+        this.props.changeText(text);
+        console.log(text);
+    }
 
     render() {
         return (
-            <div dangerouslySetInnerHTML={{__html: this.props.text.split("\n").join("<br>")}}>
+            <div>
+                <textarea value={this.props.text} onChange={this.updateText.bind(this)} className="form-control" >
+
+                </textarea>
             </div>
         )
     }
